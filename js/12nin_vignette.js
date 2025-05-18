a03537b03d00307d3d958f448c227eee815ae4a5
さきちび <sakitibi10nin@icloud.com>
2025-05-18 12:47:31 +0900

    Update 12nin_vignette.js

--- js/12nin_vignette.js
+++ js/12nin_vignette.js
@@ -1,20 +1,23 @@
-var target = document.getElementById('content');
-var uri = target.baseURI;
-const url = new URL(uri);
-const params = new URLSearchParams(window.location.search)
 let random = Math.floor(Math.random() * 11);
-function ads() {
-    if (random === 10) {
-        url.searchParams.set("ad", "google_vignette");
-    } else {
-        url.searchParams.set("ad", "none");
-    }
+
+// URLオブジェクトを現在のURLから作成
+const url = new URL(window.location.href);
+
+// 条件に応じて ad パラメータを設定
+if (random === 10) {
+    url.searchParams.set("ad", "google_vignette");
+} else {
+    url.searchParams.set("ad", "none");
 }
 
-ads();
+// 履歴を書き換えてURLを更新（リロードなしで）
+window.history.replaceState({}, '', url);
 
+// クエリパラメータを再取得
+const params = new URLSearchParams(window.location.search);
 const ad = params.get('ad');
 
-if(ad === 'google_vignette'){
+// 条件に応じて処理を実行
+if (ad === 'google_vignette') {
     console.log("!!!");
 }
