09b3c5eef8aee68416ceb8d97aa620307b3a76dc
さきちび <sakitibi10nin@icloud.com>
2025-05-18 12:39:05 +0900

    Update 12nin_vignette.js

--- js/12nin_vignette.js
+++ js/12nin_vignette.js
@@ -3,13 +3,17 @@ var uri = target.baseURI;
 const url = new URL(uri);
 const params = new URLSearchParams(window.location.search)
 let random = Math.floor(Math.random() * 11);
-if (random === 10) {
-    url.searchParams.set("ad", "google_vignette");
-} else {
-    url.searchParams.set("ad", "none");
+function ads() {
+    if (random === 10) {
+        url.searchParams.set("ad", "google_vignette");
+    } else {
+        url.searchParams.set("ad", "none");
+    }
 }
-const ad = params.get('ad');
 
+ads();
+
+const ad = params.get('ad');
 
 if(ad === 'google_vignette'){
     console.log("!!!");
