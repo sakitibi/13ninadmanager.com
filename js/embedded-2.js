f148bf095fd7265581e43b27b02fffc038395381
さきちび <sakitibi10nin@icloud.com>
2025-07-08 12:59:02 +0900

    Update embedded-2.js

--- js/embedded-2.js
+++ js/embedded-2.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
     let AdRandom = Math.floor(Math.random() * 2);
-    if (typeof document.getElementById("main-contents-bottom") !== 'undefined'){
+    if (document.getElementById("main-contents-bottom") !== null){
         const MainContentsBottom = document.getElementById("main-contents-bottom");
     	if(AdRandom === 0){
     	    MainContentsBottom.innerHTML = (`
          		<iframe class="juusannninadmanager" src="https://sakitibi-com9.webnode.jp/page/0" width="336" height="280"></iframe>
     		    <iframe class="juusannninadmanager" src="https://sakitibi-com9.webnode.jp/page/0" width="336" height="280"></iframe>
          	`);
-    	}
+        }
     }
-}
+});
