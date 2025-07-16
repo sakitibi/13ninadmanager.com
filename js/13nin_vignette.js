6929db7ccae96ade4531f8f30dac60948ef1854d
さきちび <sakitibi10nin@icloud.com>
2025-07-16 22:21:19 +0900

    Update 13nin_vignette.js

--- js/13nin_vignette.js
+++ js/13nin_vignette.js
@@ -16,7 +16,7 @@
   const url = new URL(location.href);
   const selects = [
     { src: "S7O5-dFA420", base: 6, pat1: 140 }, // 万博
-    { src: "Eh3cJyXCmBU", base: 6, pat1: 483 }, // メテヲ
+    { src: "sgVVR34sHio", base: 6, pat1: 483 }, // メテヲ
     { src: "4RhagVtxbwk", base: 6, pat1: 645 }, // Latte
     { src: "meDW3yvRlig", base: 6, pat1: 893 }, // ひなにい
     { src: "f7VBbLZey-Y", base: 6, pat1:1114 }, // めめんともり
