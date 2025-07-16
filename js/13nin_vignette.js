7e33adb906012e49a5b64fb21521d0a554368abd
さきちび <sakitibi10nin@icloud.com>
2025-07-16 16:35:07 +0900

    Update 13nin_vignette.js

--- js/13nin_vignette.js
+++ js/13nin_vignette.js
@@ -17,11 +17,11 @@
   const selects = [
     { src: "S7O5-dFA420", base: 6, pat1: 140 }, // 万博
     { src: "Eh3cJyXCmBU", base: 6, pat1: 483 }, // メテヲ
-    { src: "4RhagVtxbwk", base: 6, pat1: 735 }, // Latte
-    { src: "meDW3yvRlig", base: 6, pat1: 934 }, // ひなにい
+    { src: "4RhagVtxbwk", base: 6, pat1: 645 }, // Latte
+    { src: "meDW3yvRlig", base: 6, pat1: 893 }, // ひなにい
     { src: "f7VBbLZey-Y", base: 6, pat1:1114 }, // めめんともり
     { src: "_LhLyW4Yk-M", base: 6, pat1: 32 }, // 原神 v5.7
-    { src: "O7iVIK_tLpE", base: 6, pat1: 578 }, // みぞれ
+    { src: "O7iVIK_tLpE", base: 6, pat1: 567 }, // みぞれ
     { src: "7eJTeiG83Uo", base: 6, pat1: 17 } // Wecars
   ];
 
