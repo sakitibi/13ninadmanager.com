b91feb4891e444dfbf7dff859ff17fea296f9373
さきちび <sakitibi10nin@icloud.com>
2025-07-18 19:49:07 +0900

    Update 13nin_vignette.js

--- js/13nin_vignette.js
+++ js/13nin_vignette.js
@@ -17,7 +17,7 @@
   const selects = [
     { src: "S7O5-dFA420", base: 6, pat1: 140 }, // 万博
     { src: "sgVVR34sHio", base: 6, pat1: 483 }, // メテヲ
-    { src: "aofM3pwazmg", base: 6, pat1: 645 }, // Latte
+    { src: "aofM3pwazmg", base: 6, pat1: 707 }, // Latte
     { src: "meDW3yvRlig", base: 6, pat1: 893 }, // ひなにい
     { src: "f7VBbLZey-Y", base: 6, pat1:1114 }, // めめんともり
     { src: "_LhLyW4Yk-M", base: 6, pat1: 32 }, // 原神 v5.7
