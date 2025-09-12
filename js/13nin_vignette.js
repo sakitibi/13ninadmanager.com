2dbfb49135ec289e387e97c4f2cba48566313456
さきちび <sakitibi10nin@icloud.com>
2025-09-12 17:22:20 +0900

    Update 13nin_vignette.js

--- js/13nin_vignette.js
+++ js/13nin_vignette.js
@@ -25,7 +25,7 @@ function pickAdHooks(IsTrainBuildersHooks){
   const selects = [
     { src: "S7O5-dFA420", base: 6, pat1: 140, publisher: '公益社団法人2025年日本国際博覧会協会' }, // 万博
     { src: "MC_zzXNXfKI", base: 6, pat1: 489, publisher: 'メテヲs7のゆっくり実況部屋' }, // メテヲ
-    { src: "ZlitSSmdQ_8", base: 6, pat1: 613, publisher: 'Latte' }, // Latte
+    { src: "ogdoFQYxfxE", base: 6, pat1: 617, publisher: 'Latte' }, // Latte
     { src: "8Elvq9YfGNc", base: 6, pat1: 876, publisher: 'ヒナの隠れ家' }, // ひなにい
     { src: "7OnV7KgY2U8", base: 6, pat1:1070, publisher: 'めめんともり' }, // めめんともり
     { src: "7NLwNj_hYk4", base: 6, pat1: 31, publisher: '原神-Genshin-公式' }, // 原神 v5.8
