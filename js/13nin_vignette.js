e831a9802f27ca21c5667a0960c47ad8c4a094d5
さきちび <sakitibi10nin@icloud.com>
2025-10-07 11:24:45 +0900

    Update 13nin_vignette.js

--- js/13nin_vignette.js
+++ js/13nin_vignette.js
@@ -31,7 +31,7 @@ function pickAdHooks(IsTrainBuildersHooks){
   await fetched();
   const selects = [
     { src: srcs[0], pat1: 140, publisher: '公益社団法人2025年日本国際博覧会協会' }, // 万博
-    { src: srcs[1], pat1: 548, publisher: 'メテヲs7のゆっくり実況部屋' }, // メテヲ
+    { src: srcs[1], pat1: 512, publisher: 'メテヲs7のゆっくり実況部屋' }, // メテヲ
     { src: srcs[2], pat1: 745, publisher: 'Latte' }, // Latte
     { src: srcs[3], pat1: 840, publisher: 'ヒナの隠れ家' }, // ひなにい
     { src: srcs[4], pat1:1028, publisher: 'めめんともり' }, // めめんともり
