c4afc81ea3de60afb8fc276c00e4c94b5c8f639a
さきちび <sakitibi10nin@icloud.com>
2025-11-10 22:49:01 +0900

    Update publisher pat1 value for メテヲ

--- js/13nin_vignette.js
+++ js/13nin_vignette.js
@@ -30,7 +30,7 @@ function pickAdHooks(IsTrainBuildersHooks){
   }
   await fetched();
   const selects = [
-    { src: srcs[0], pat1: 506, publisher: 'メテヲs7のゆっくり実況部屋' }, // メテヲ
+    { src: srcs[0], pat1: 518, publisher: 'メテヲs7のゆっくり実況部屋' }, // メテヲ
     { src: srcs[1], pat1: 737, publisher: 'Latte' }, // Latte
     { src: srcs[2], pat1: 929, publisher: 'ヒナの隠れ家' }, // ひなにい
     { src: srcs[3], pat1: 996, publisher: 'めめんともり' }, // めめんともり
