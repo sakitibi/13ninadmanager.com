394a9f83b63ba140fcd00446cd5592e10821a499
さきちび <sakitibi10nin@icloud.com>
2025-11-29 16:49:01 +0900

    Update 13nin_vignette.js

--- js/13nin_vignette.js
+++ js/13nin_vignette.js
@@ -33,7 +33,7 @@ function pickAdHooks(IsTrainBuildersHooks){
     { src: srcs[0], pat1: 486, publisher: 'メテヲs7のゆっくり実況部屋' }, // メテヲ
     { src: srcs[1], pat1: 723, publisher: 'Latte' }, // Latte
     { src: srcs[2], pat1: 938, publisher: 'ヒナの隠れ家' }, // ひなにい
-    { src: srcs[3], pat1:1042, publisher: 'めめんともり' }, // めめんともり
+    { src: srcs[3], pat1:1051, publisher: 'めめんともり' }, // めめんともり
     { src: srcs[4], pat1: 32, publisher: '原神-Genshin-公式' }, // 原神 Luna ⅠⅠ
     { src: srcs[5], pat1: 511, publisher: 'みぞれch' }, // みぞれ
     { src: srcs[6], pat1: 18, publisher: 'WECARS ウィーカーズ' }, // Wecars1
