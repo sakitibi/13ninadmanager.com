938d9b5e18bbab64ded9720dd88d25bfd56d43c7
さきちび <sakitibi10nin@icloud.com>
2025-12-14 08:52:34 +0900

    Update 13nin_vignette.js

--- js/13nin_vignette.js
+++ js/13nin_vignette.js
@@ -40,7 +40,7 @@ function pickAdHooks(IsTrainBuildersHooks){
     { src: srcs[7], pat1: 18, publisher: 'WECARS ウィーカーズ' }, // Wecars2
     { src: srcs[8], pat1: 18, publisher: 'WECARS ウィーカーズ' }, // Wecars3
     { src: srcs[9], pat1: 18, publisher: 'WECARS ウィーカーズ' }, // Wecars4
-    { src: srcs[10], pat1: 868, publisher: 'ゆっくりウパパロン' }, // ゆっくりウパパロン
+    { src: srcs[10], pat1: 938, publisher: 'ゆっくりウパパロン' }, // ゆっくりウパパロン
     { src: srcs[11], pat1: 728, publisher: '茶子 / ゆっくり実況' }, // 茶子 / ゆっくり実況
     { src: srcs[12], pat1: 18, publisher: 'WECARS ウィーカーズ' }, // Wecars5
     { src: srcs[13], pat1: 18, publisher: 'WECARS ウィーカーズ' }, // Wecars6
