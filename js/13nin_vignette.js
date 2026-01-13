488a6a470817ec4ad1d630a97f2ab6bf10e0b293
さきちび <sakitibi10nin@icloud.com>
2026-01-13 11:42:28 +0900

    Update 13nin_vignette.js

--- js/13nin_vignette.js
+++ js/13nin_vignette.js
@@ -30,7 +30,7 @@ function pickAdHooks(IsTrainBuildersHooks){
   }
   await fetched();
   const selects = [
-    { src: srcs[0], pat1: 500, publisher: 'メテヲs7のゆっくり実況部屋' }, // メテヲ
+    { src: srcs[0], pat1: 492, publisher: 'メテヲs7のゆっくり実況部屋' }, // メテヲ
     { src: srcs[1], pat1: 665, publisher: 'Latte' }, // Latte
     { src: srcs[2], pat1: 893, publisher: 'ヒナの隠れ家' }, // ひなにい
     { src: srcs[3], pat1: 1100, publisher: 'めめんともり' }, // めめんともり
