4aca5c44c4e1f71a416a51e7b1a732b6e7d61f6f
sakitibi <sakitibi10nin@icloud.com>
2026-04-09 23:40:22 +0900

    Merge remote changes

--- 13nin_vignette_src/src/ad_engine.cpp
+++ 13nin_vignette_src/src/ad_engine.cpp
@@ -1,10 +1,11 @@
 #include "ad_engine.hpp"
 #include "dom_utils.hpp"
+#include "ad_data.hpp"
 #include <emscripten/val.h>
 #include <algorithm>
 #include <string>
 #include <vector>
-#include <iostream> // std::cout用（任意）
+#include <iostream>
 
 using namespace emscripten;
 
@@ -19,28 +20,6 @@ AdEngine::AdEngine() : isAdPlaying(false) {
 
 void AdEngine::setMetadata(val srcs, val times) {
     selects.clear();
-    const std::string site_baseurl = "https://13ninadmanagerclick.vercel.app/ck/";
-    
-    struct Meta { std::string pub; std::string key; };
-    std::vector<Meta> metaList = {
-        {"メテヲs7のゆっくり実況部屋", "9SrU_djJLoOmVc4UX0k8CM7hElCgVGFZnM9cDDQ9byD5q5Isz-yKD1lO-FTYajEawD6o-LUCdDwGzqRzab4I7R1QHCJQA15srIODy2Qk22RQtDM1uWr5TyW3n-lfMgnjyN0zm-fnTvqdV1KH2jQJpGd4B9zQXu"},
-        {"Latte", "QxmTxWxnD4zsAHrRTDLW4ObxvHec4diHfmoCDhYhkWAW5XgFacyKtUAkfigfUTE48dceqhY2yBmCCgWZOdvfl_GWjAPUEoFMShziSNGH-U2swvis6crubVcdIMs-Q72cgcSs_S3av6rCC3UIsXyMKsYe1qqEHez4trJ"},
-        {"ヒナの隠れ家", "IwV62n9DIUgcOfQSYSPBSWPsnSAypR-NJG6LVjSzRImGzvrKjyVEqoM7hC7Gil-O7QJ_3bj7WnpOKQEvkcyu7Nxtl4tgT8GdrVl5cdamSzhmEufQKkpDr4YwIzDZ79I54aW42c_kQZsQZvmCCw165t4SUmMFh0bP7uePJlyCaXBB3mtHuMyroZk9"},
-        {"めめんともり", "TKQVLhl2WhWwQ82S6OqY3-vXny3k3cs-ORby3iXuGvRjwtU6L8kVnCrFv2QHgoop-KxTovTdn6d_QRjqZFWAI7WsHmLLOTkVhY2tAujmyUouoBcNUOzwfj"},
-        {"原神-Genshin-公式", "wfT-jXAd_ayThE7j615gVbniRpgB2cGb02osMmyOhy9DvK1_kzZZDIe60hxbk8OHK2tguZ7xac83Yl26qaVm4ZHN46GKdf6k5kS5q1aH7Q"},
-        {"みぞれch", "F0qm-fkYK8ZZN8jnr077MMu4Zl9E8rhmEeimB_ax8KJcy6UqhzbhqgbG6FtPsr3G3kerRXU7KFP-39-V0QRb7CNZojGyK2Xe2pXsDc7W2m6nVjZmcYeHRL"},
-        {"WECARS ウィーカーズ", "6GLxlfFs2rUuT3zTquIAsZIGprPCrAwxtErbj5acWVTB3_-Z-07lsYt9Kx4_5fJaMp5o4Fe2FkyP7dUfWnAgyiAhhY44rCl6FDAJEXfy20Wj4Ce0YfpIT8jz5f5zuTIkWEsU6AU0ka"},
-        {"WECARS ウィーカーズ", "6GLxlfFs2rUuT3zTquIAsZIGprPCrAwxtErbj5acWVTB3_-Z-07lsYt9Kx4_5fJaMp5o4Fe2FkyP7dUfWnAgyiAhhY44rCl6FDAJEXfy20Wj4Ce0YfpIT8jz5f5zuTIkWEsU6AU0ka"},
-        {"WECARS ウィーカーズ", "6GLxlfFs2rUuT3zTquIAsZIGprPCrAwxtErbj5acWVTB3_-Z-07lsYt9Kx4_5fJaMp5o4Fe2FkyP7dUfWnAgyiAhhY44rCl6FDAJEXfy20Wj4Ce0YfpIT8jz5f5zuTIkWEsU6AU0ka"},
-        {"WECARS ウィーカーズ", "6GLxlfFs2rUuT3zTquIAsZIGprPCrAwxtErbj5acWVTB3_-Z-07lsYt9Kx4_5fJaMp5o4Fe2FkyP7dUfWnAgyiAhhY44rCl6FDAJEXfy20Wj4Ce0YfpIT8jz5f5zuTIkWEsU6AU0ka"},
-        {"ゆっくりウパパロン", "Flhv9cz5_Nl143zsAcuVhkyv1NicSoU4tv9bfMQJdbVkogsEzROVkvZY0na8rZg0-klWelCsgeXC2VWJDW9MO5dIsTp4jD6ZSbw2hTpG7RD_UhOegX2zzXfxFGvDvcF3Ed0Otsg7Tf-"},
-        {"茶子 / ゆっくり実況", "DFtDSz_Gd9ln2mtRBn3jGzy8gk8ofJpOEsD5zUEV87csq3epjwwziPdP6Lf6vbjzeBOLC61WPfqPMI4NMg56-MT-nLYX9RkPfBTkf3XYxeW56l3krEwjgmI"},
-        {"WECARS ウィーカーズ", "6GLxlfFs2rUuT3zTquIAsZIGprPCrAwxtErbj5acWVTB3_-Z-07lsYt9Kx4_5fJaMp5o4Fe2FkyP7dUfWnAgyiAhhY44rCl6FDAJEXfy20Wj4Ce0YfpIT8jz5f5zuTIkWEsU6AU0ka"},
-        {"WECARS ウィーカーズ", "6GLxlfFs2rUuT3zTquIAsZIGprPCrAwxtErbj5acWVTB3_-Z-07lsYt9Kx4_5fJaMp5o4Fe2FkyP7dUfWnAgyiAhhY44rCl6FDAJEXfy20Wj4Ce0YfpIT8jz5f5zuTIkWEsU6AU0ka"},
-        {"WECARS ウィーカーズ", "6GLxlfFs2rUuT3zTquIAsZIGprPCrAwxtErbj5acWVTB3_-Z-07lsYt9Kx4_5fJaMp5o4Fe2FkyP7dUfWnAgyiAhhY44rCl6FDAJEXfy20Wj4Ce0YfpIT8jz5f5zuTIkWEsU6AU0ka"},
-        {"WECARS ウィーカーズ", "6GLxlfFs2rUuT3zTquIAsZIGprPCrAwxtErbj5acWVTB3_-Z-07lsYt9Kx4_5fJaMp5o4Fe2FkyP7dUfWnAgyiAhhY44rCl6FDAJEXfy20Wj4Ce0YfpIT8jz5f5zuTIkWEsU6AU0ka"},
-        {"あかさかの箱", "CcRpgdMOX_yg1YHmtNaeR8unSC4ob_apwt64QDEP2EaWP3MNp5G4P4ZP5qF1ZGHSUpkck5GjxOf78KcZExWdowtCnIHj3XvcKYC17SKwCN9MkuKj660oQMomuvNtJB8iOF8_H400vG_ixVlEW94gN3bnH6XH0BX1zuzUB4ytgxiafpwb_x"}
-    };
 
     size_t len = srcs["length"].as<size_t>();
     for (size_t i = 0; i < len; ++i) {
@@ -48,9 +27,9 @@ void AdEngine::setMetadata(val srcs, val times) {
         ad.src = srcs[i].as<std::string>();
         ad.pat1 = times[i].as<int>();
         
-        if (i < metaList.size()) {
-            ad.publisher = metaList[i].pub;
-            ad.site = site_baseurl + metaList[i].key;
+        if (i < AdData::AD_META_LIST.size()) {
+            ad.publisher = AdData::AD_META_LIST[i].pub;
+            ad.site = AdData::SITE_BASEURL + AdData::AD_META_LIST[i].key;
         } else {
             ad.publisher = "不明";
             ad.site = "./";
